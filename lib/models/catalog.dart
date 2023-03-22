5d1a8bfd422b9f907f8596359facbf03edd16516
manthanpanchal2002 <manthanppanchal21@gnu.ac.in>
2023-03-22 23:19:38 +0530

    Day 8 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,12 +1,31 @@
-class Items {
-  final String id;
+class CatalogModels {
+  static final items = [
+    Item(
+        id: 1001,
+        name: 'iPhone 14 Pro',
+        desc: 'Apple',
+        price: 129900,
+        color: '#33505a',
+        image: 'assets/images/iphone_14_Pro_img.jpeg'),
+    Item(
+        id: 1002,
+        name: 'iPhone 14',
+        desc: 'Apple',
+        price: 79900,
+        color: '#33505a',
+        image: 'assets/images/iphone_14_img.jpeg'),
+  ];
+}
+
+class Item {
+  final int id;
   final String name;
   final String desc;
   final num price;
   final String color;
   final String image;
 
-  Items(
+  Item(
       {required this.id,
       required this.name,
       required this.desc,
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 class MyTheme {
   static ThemeData LightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.lightBlue,
-      fontFamily: GoogleFonts.poppins().fontFamily,
+      // fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
         color: Colors.white,
         elevation: 0.0,
