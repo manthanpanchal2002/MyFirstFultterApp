221b774d6666f3ae0d0442f73254857b6bf1baa5
Manthan Panchal <manthanppanchal21@gnu.ac.in>
2023-03-31 15:20:56 +0530

    Day 15 completed

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,6 +1,11 @@
 import 'package:first_flutter_application/models/catalog.dart';
 
 class CartModel {
+  // singleton class
+  static final cartModel = CartModel._internal();
+  CartModel._internal();
+  factory CartModel()=> cartModel;
+  // ----------------
   late CatalogModels _catalog;
   final List<int> _itemId = [];
   CatalogModels get catalog => _catalog;
